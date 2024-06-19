e62207c34fe303bb47d6960df163bb70eadb2fe1
EC2 Default User <ec2-user@ip-172-31-48-96.ap-northeast-2.compute.internal>
2024-06-19 09:36:31 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import UpperPage from './page/UpperPage'
 import axios from 'axios';
-
 import { makeStyles } from '@material-ui/core/styles';
-
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { Button, IconButton } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  TextField,
+  CircularProgress,
+} from '@material-ui/core';
 import CloudIcon from '@material-ui/icons/Cloud';
-import TextField from '@material-ui/core/TextField';
-
+import UpperPage from './page/UpperPage'; // Adjust path if needed
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   searchRoot: {
+    display: 'flex',
+    alignItems: 'center',
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
     },
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -32,27 +32,41 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  content: {
+    padding: theme.spacing(2),
+  },
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing(2),
+  },
 }));
 
 function App(props) {
   const classes = useStyles();
-  // const { sections } = props;
-  const [ data, setState ] = useState({outcome: []});
+  const [data, setData] = useState({ outcome: [] });
   const [query, setQuery] = useState('eks');
-  const [search, setSearch] = useState('eks');
+  const [loading, setLoading] = useState(false);
 
-  var url = `http://k8s-eksdemogroup-e0353f9ab7-1386321306.ap-northeast-2.elb.amazonaws.com/contents/${search}`
-
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const url = `http://k8s-eksdemogroup-e0353f9ab7-1386321306.ap-northeast-2.elb.amazonaws.com/contents/${query}`;
+      const result = await axios(url);
+      setData(result.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setData({ outcome: [] }); // Reset data on error
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(url);
-      setState(result.data);
-    };
     fetchData();
-    // eslint-disable-next-line
-  }, [search]);
-  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
   return (
     <div className={classes.root}>
       <AppBar position="static" style={{ background: '#2E3B55' }}>
@@ -60,57 +74,47 @@ function App(props) {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <CloudIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            align="center"
-            className={classes.title}
-          >
-              return (
-    <div className={classes.root}>
-      <AppBar position="static" style={{ background: '#2E3B55' }}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <CloudIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            align="center"
-            className={classes.title}
-          >
+          <Typography variant="h6" className={classes.title}>
             EKS DEMO Blog version 1
           </Typography>
-          {new Date().toLocaleTimeString()}
-        </Toolbar>
-      </AppBar>
-      <br/>
-
-            EKS DEMO Blog
+          <Typography variant="body2">
+            {new Date().toLocaleTimeString()}
           </Typography>
-          {new Date().toLocaleTimeString()}
         </Toolbar>
       </AppBar>
-      <br/>
 
-      <UpperPage key={1} />
-      <br/>
-      
-      <form className={classes.searchRoot} noValidate autoComplete="off">
-        <TextField
-          id="standard-basic"
-          label="Enter your keyword to search"
-          type="text"
-          value={query}
-          onChange={event => setQuery(event.target.value)}
-        />
-        <Button onClick={() => setSearch(query)}> Click </Button>
-      </form>
-      <ul>
-      {data.outcome.map( item => (
-        <li key={item.url}>
-          <a href={item.url}>{item.title}</a><br/>
-        </li>
-      ))}
-      </ul>
+      <div className={classes.content}>
+        <UpperPage key={1} />
+        <br />
+
+        <form className={classes.searchRoot} noValidate autoComplete="off">
+          <TextField
+            id="standard-basic"
+            label="Enter your keyword to search"
+            type="text"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Button variant="contained" color="primary" onClick={fetchData}>
+            Search
+          </Button>
+        </form>
+
+        {loading ? (
+          <div className={classes.loading}>
+            <CircularProgress />
+          </div>
+        ) : (
+          <ul>
+            {data.outcome.map((item) => (
+              <li key={item.url}>
+                <a href={item.url}>{item.title}</a>
+                <br />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
